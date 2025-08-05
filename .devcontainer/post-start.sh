fb70616e7b5aa98bda0a291825f240237c42eab7
joshDynatrace <joshua.hendrick@dynatrace.com>
2025-08-05 10:43:05 -0700

    Update post-start.sh

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -5,6 +5,6 @@ source /workspaces/$RepositoryName/.devcontainer/util/functions.sh
 
 #This is for professors
 #exposeMkdocs
-exposeBugZapper
+exposeBugZapperApp
 
 printInfoSection "Your dev.container finished starting up"
