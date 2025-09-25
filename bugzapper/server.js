a1b4aa1f014c7ec8f83a4a233b2622db438e7a6b
joshDynatrace <joshua.hendrick@dynatrace.com>
2025-09-25 14:49:01 -0700

    Removed comment as it was causing a ReferenceError

--- bugzapper/server.js
+++ bugzapper/server.js
@@ -55,7 +55,6 @@ app.post('/api/scores', (req, res) => {
 
 // Clear all scores
 app.get('/api/clearScores', (req, res) => {
-  console.log('Scores before clearing:', scores);
   let scores = [];
   console.log('All scores cleared successfully');
   res.json({ message: 'All scores cleared successfully' });
